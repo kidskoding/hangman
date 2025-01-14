615b63eb6386b741472ad3aa33883b411e2a01fc
Anirudh Konidala <kidskodingclub@gmail.com>
2025-01-14 11:42:04 -0600

    Random word can be retrieved from a words.txt file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+use rand::seq::SliceRandom;
+
 fn main() {
     println!("Hangman in Rust!");
+    let path = fs::read_to_string("./src/hangman.txt");
+    match path {
+        Ok(contents) => {
+            let lines: Vec<&str> = contents.lines().collect();
+            let mut rng = rand::thread_rng();
+            let random_word = lines.choose(&mut rng);
+            match random_word {
+                Some(random_word) => {
+                    println!("{random_word}");
+                }
+                None => panic!("failed to retrieve a random word. \
+                    ensure the ./src/hangman.txt file is available")
+            }
+        }
+        Err(e) => panic!("failed to read file: {e}"),
+    }
 }
